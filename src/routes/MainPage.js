649ba8ac6eed62b825ac794b997d449e71ae0bf3
Oliver Winkler <de.oliver.winkler@gmail.com>
2022-06-25 15:16:20 +0200

    :sparkles: Added better start page and deleted some standard files

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,5 +1,19 @@
 export default function MainPage() {
     return(
-        <h1>MainPage</h1>
+        <div className="container">
+            <div className="card">
+            <div className="card-header">
+                <h1 className = "card-title text-center">Willkommen bei winkloidShare!</h1>
+            </div>
+            <div className="card-body">
+            <p className="card-text">Gelangen Sie hier zur Upload-Seite: </p>
+            <a className="btn btn-primary col-12" href="upload">Upload-Seite</a>
+            </div>
+            <div className="card-footer">
+            <p>Administratoren hier entlang: </p>
+            <a className="btn btn-primary col-12" href="administrator">Administration</a>
+            </div>
+            </div>
+        </div>
     );
 }
